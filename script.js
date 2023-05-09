7dbdc51c6538e012a4b6d589ebfa879f89f294f3
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2023-05-09 14:09:24 +0530

    Added random rgb color generating function

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ for(let i=0;i<256;i++){
     div.classList.add('pixel');
     container.appendChild(div);
 }
+let pixel=document.querySelectorAll('.pixel')
+pixel.forEach(div=>{
+    div.addEventListener('mouseenter',rgb);
+});
+function rgb(event){
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    event.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
