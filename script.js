38d217bc4c6fb5418028fb36c6a311f7e518c401
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2023-05-09 15:20:46 +0530

    Added Pixel Button

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const container = document.querySelector('.container');
-let number = prompt("how many pixels in a row");
-let total_pixel = number * number;
-let width_height = 640 / number;
-for (let i = 0; i < total_pixel; i++) {
-  const div = document.createElement('div');
-  div.classList.add('pixel');
-  div.setAttribute('style', `width: ${width_height}px; height: ${width_height}px`);
-  container.appendChild(div);
+const button=document.querySelector('button');
+button.addEventListener('click',fun_prompt);
+function fun_prompt(event){
+    let number = prompt("How many pixels in a row?");
+    let total_pixel = number * number;
+    let width_height = 640 / number;
+    new_sketch(total_pixel,width_height);
 }
-
-let pixel=document.querySelectorAll('.pixel')
-pixel.forEach(div=>{
+function new_sketch(total_pixel,width_height){
+    for (let i = 0; i < total_pixel; i++) {
+    const div = document.createElement('div');
+    div.classList.add('pixel');
+    div.setAttribute('style', `width: ${width_height}px; height: ${width_height}px`);
+    container.appendChild(div);
     div.addEventListener('mouseenter',rgb);
-});
+    }
+}
 function rgb(event){
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
